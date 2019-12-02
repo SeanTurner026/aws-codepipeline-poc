9c3ff0a5a198c3fc0c51b4eb22ac4f1addecaee0
SeanTurner026 <sean.turner026@gmail.com>
2019-12-02 13:13:56 +0700

    Simplified application

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-from flask import Flask, escape, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return "Flask + Docker demo!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
