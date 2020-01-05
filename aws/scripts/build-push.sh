57dab582e14fc0c71f78615f3e58b9fe89ecc4f9
SeanTurner026 <sean.turner026@gmail.com>
2020-01-05 11:44:13 -1000

    Changed path to correspond with additon of docker directory

--- aws/scripts/build-push.sh
+++ aws/scripts/build-push.sh
@@ -2,6 +2,6 @@
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION ../../Dockerfile
+docker build -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION ~/docker/Dockerfile
 
 docker push seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
