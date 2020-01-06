8a1b808334b143ea130238bfd763f0c9c38d05e6
SeanTurner026 <sean.turner026@gmail.com>
2020-01-06 13:24:23 -1000

    Replaced hardcoded hub.docker account with environment variable

--- aws/scripts/build-push.sh
+++ aws/scripts/build-push.sh
@@ -3,7 +3,7 @@
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
 docker build . \
-    -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+    -t $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
     -f $CODEBUILD_SRC_DIR/docker/Dockerfile
 
-docker push seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
+docker push $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
