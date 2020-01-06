e6bfcffc6431c4c46f1d1659db29190401482aaf
SeanTurner026 <sean.turner026@gmail.com>
2020-01-05 15:12:17 -1000

    Login via stdin

--- aws/scripts/build-push.sh
+++ aws/scripts/build-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
-docker build -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION aws/docker/Dockerfile
+docker build -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION ~/aws/docker/Dockerfile
 
 docker push seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
