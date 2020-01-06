a443e02377d608129b1e4db9df9f2b6c41c068b7
SeanTurner026 <sean.turner026@gmail.com>
2020-01-05 16:01:42 -1000

    Fixed context

--- aws/scripts/build-push.sh
+++ aws/scripts/build-push.sh
@@ -2,6 +2,8 @@
 
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
-docker build -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION $CODEBUILD_SRC_DIR/docker/Dockerfile
+docker build . \
+    -t seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+    -f $CODEBUILD_SRC_DIR/docker/Dockerfile
 
 docker push seanturner026/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
