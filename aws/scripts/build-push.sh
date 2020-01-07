3eeabda6e59ce0f19372d77606046ccbef211c39
SeanTurner026 <sean.turner026@gmail.com>
2020-01-06 17:49:05 -1000

    Added the latest tag

--- aws/scripts/build-push.sh
+++ aws/scripts/build-push.sh
@@ -6,4 +6,8 @@ docker build . \
     -t $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
     -f $CODEBUILD_SRC_DIR/docker/Dockerfile
 
+docker tag $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+           $DOCKER_USERNAME/aws-codepipeline-poc:latest
+
 docker push $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
+docker push $DOCKER_USERNAME/aws-codepipeline-poc:latest
