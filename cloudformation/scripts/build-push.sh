9b2dc4c3ca4bffafae5308937201ef64eb4020ad
SeanTurner026 <sean.turner026@gmail.com>
2020-01-24 10:00:55 +1300

    Updated for ecr

--- cloudformation/scripts/build-push.sh
+++ cloudformation/scripts/build-push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+export GIT_SHA=$CODEBUILD_RESOLVED_SOURCE_VERSION
+export AWS_ACCOUNT_ID=$CODEBUILD_WEBHOOK_ACTOR_ACCOUNT_ID
 
 docker build . \
-    -t $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
+    -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-codepipeline-poc:$GIT_SHA \
     -f $CODEBUILD_SRC_DIR/docker/Dockerfile
 
-docker tag $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION \
-           $DOCKER_USERNAME/aws-codepipeline-poc:latest
+docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-codepipeline-poc:$GIT_SHA \
+           $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-codepipeline-poc:latest
 
-docker push $DOCKER_USERNAME/aws-codepipeline-poc:$CODEBUILD_RESOLVED_SOURCE_VERSION
-docker push $DOCKER_USERNAME/aws-codepipeline-poc:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-codepipeline-poc:$GIT_SHA
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-codepipeline-poc:latest
